fc29fa423e054733fb4387ad97126cf173e33bbb
TheHitMan7 <krtik.vrma@gmail.com>
2022-10-29 11:00:13 +0530

    Initial Google Messaging Support
    Signed-off-by: TheHitMan7 <krtik.vrma@gmail.com>

---
+++ Messaging/70-messaging.sh
@@ -0,0 +1,48 @@
+#!/sbin/sh
+#
+# This file is part of The BiTGApps Project
+
+# ADDOND_VERSION=3
+
+if [ -z "$backuptool_ab" ]; then
+  SYS="$S"
+  TMP=/tmp
+else
+  SYS="/postinstall/system"
+  TMP="/postinstall/tmp"
+fi
+
+. /tmp/backuptool.functions
+
+list_files() {
+cat <<EOF
+app/Messaging/Messaging.apk
+app/Messaging/split_config.arm64_v8a.apk
+app/Messaging/split_config.en.apk
+app/Messaging/split_config.xxhdpi.apk
+EOF
+}
+
+case "$1" in
+  backup)
+    list_files | while read FILE DUMMY; do
+      backup_file $S/"$FILE"
+    done
+  ;;
+  restore)
+    list_files | while read FILE REPLACEMENT; do
+      R=""
+      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+      [ -f "$C/$S/$FILE" ] && restore_file $S/"$FILE" "$R"
+    done
+    for f in $SYS $SYS/product $SYS/system_ext; do
+      find $f -type d -name '*Messaging*' -exec rm -rf {} +
+      find $f -type d -name '*messaging*' -exec rm -rf {} +
+    done
+    for i in $(list_files); do
+      chown root:root "$SYS/$i" 2>/dev/null
+      chmod 644 "$SYS/$i" 2>/dev/null
+      chmod 755 "$(dirname "$SYS/$i")" 2>/dev/null
+    done
+  ;;
+esac
